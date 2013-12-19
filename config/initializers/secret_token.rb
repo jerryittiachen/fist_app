d395aed5f9d82847bb8661091137abca390b40e3
Jerry Ittiachen <jerry.ittiachen@gmail.com>
2013-12-19 15:01:47 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FistApp::Application.config.secret_token = 'fbb73b9b7f1e8e5f010c7d1be74a8c1930b32b31e17c0b48b855c486291b9b16762dc55c9e7768202743171abd30d3c423032e83f5be6bc0dd73c554f63188c8'
